f447f90604ff1d5a50d5653f0ffdb09e67949aef
Don Diego de la Vega <stonerzorro@outlook.com>
2026-02-22 22:56:46 +0100

    Path alias updated to '.'

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    },
+  },
   server: {
     port: 3000,
     host: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000', // Point to the same server
-        changeOrigin: true,
-        secure: false,
-      }
-    }
   }
 })
